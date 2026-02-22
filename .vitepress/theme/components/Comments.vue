193b6243dc79c0a11200517055d3945bb9e509ff
maoist2013 <maoist2013@proton.me>
2026-02-22 14:28:02 +0800

    fix comment

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -1,32 +1,44 @@
 <script setup lang="ts">
 import { Md5 } from 'ts-md5';
-import { onContentUpdated, useRouter } from "vitepress";
+import { onContentUpdated, useData } from "vitepress";
 import Giscus from '@giscus/vue';
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
-let pathname = ref(location.pathname);
-let MD5pathname = ref(Md5.hashStr(pathname));
-onContentUpdated(() => {
-  pathname.value = location.pathname;
-  MD5pathname.value = Md5.hashStr(pathname.value);
-});
+const { page } = useData();
 
+const pathname = ref('');
+const giscusTerm = ref('');
+
+const updatePath = () => {
+  if (typeof window !== 'undefined') {
+    pathname.value = location.pathname;
+    // extendPageData 已保证 title 有值
+    const title = page.value.title || 'Untitled'; 
+    const md5 = Md5.hashStr(pathname.value) as string;
+    giscusTerm.value = `${title} - ${md5}`;
+  }
+};
+
+onMounted(() => updatePath());
+onContentUpdated(() => updatePath());
 </script>
+
 <template>
-  <Giscus id="comments"
-    repo="maoist2009/MLMistREVOLUTIONagain"
-    repo-id="R_kgDON1IXoA"
-    data-category="Show and tell"
-    data-category-id="DIC_kwDON1IXoM4CoUiZ"
-    mapping="specific"
-    :term="MD5pathname"
-    reactions-enabled="1"
-    emit-metadata="0"
-    input-position="top"
-    theme="preferred_color_scheme"
-    lang="zh-CN"
-    crossorigin="anonymous"
-    :key="MD5pathname"
-    async />
+  <ClientOnly>
+    <Giscus 
+      repo="maoist2009/MLMistREVOLUTIONagain"
+      repo-id="R_kgDON1IXoA"
+      category="Website"
+      category-id="DIC_kwDON1IXoM4C21cd"
+      mapping="specific"
+      :term="giscusTerm"
+      :key="giscusTerm"
+      reactions-enabled="1"
+      emit-metadata="0"
+      input-position="top"
+      theme="preferred_color_scheme"
+      lang="zh-CN"
+      crossorigin="anonymous"
+    />
+  </ClientOnly>
 </template>
-<style scoped></style>
